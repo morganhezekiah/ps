72faccc194b62c543aadedbe14d6c4cd4a54947e
cyba <morganhezekiah59@gmail.com>
2021-12-17 02:34:25 +0100

    first commit

--- psApi/settings.py
+++ psApi/settings.py
@@ -133,8 +133,8 @@ REST_FRAMEWORK = {
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD=os.environ["EMAIL_PASSWORD"]
-EMAIL_HOST_USER=os.environ["EMAIL_ADDRESS"]
+EMAIL_HOST_PASSWORD="wmiixksozsqletoc"
+EMAIL_HOST_USER="morganhezekiah11@gmail.com"
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
